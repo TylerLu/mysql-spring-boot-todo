4a31c26587310101f02e47983c5425230f8d5035
Tyler Lu <admin@tyler.lu>
2018-10-25 15:24:24 +0800

    Specify settings file

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,3 @@
 set MAVEN_USER_HOME=D:\home\site\deployments\tools\maven
 set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
-mvnw clean install -Ddir=D:\home\site\wwwroot\webapps
+mvnw -s .mvn\settings.xml clean install -Ddir=D:\home\site\wwwroot\webapps
