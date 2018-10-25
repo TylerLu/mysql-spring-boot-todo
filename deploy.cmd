72b42ff90b9fa432649e02945e508e42fa1695ba
Tyler Lu <admin@tyler.lu>
2018-10-25 13:51:20 +0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,3 @@
-set M2_HOME=D:\home\site\deployments\tools
+set MAVEN_USER_HOME=D:\home\site\deployments\tools\mvnw
 set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
 mvnw clean install -Ddir=D:\home\site\wwwroot\webapps
