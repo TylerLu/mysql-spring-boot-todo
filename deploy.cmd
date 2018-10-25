d3753ae213909bbe7744bf190b25fd5dbede3d53
Tyler Lu <admin@tyler.lu>
2018-10-25 16:09:40 +0800

    Remove ROOT folder

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,4 @@
 set MAVEN_USER_HOME=D:\home\site\deployments\tools\maven
 set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
+rm -r ROOT
 mvnw -s .mvn\settings.xml clean install -Ddir=D:\home\site\wwwroot\webapps
